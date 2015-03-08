206a58ae50ae77f257ea0c694cc8479962f5e513
Adam Obeng <github@binaryeagle.com>
2015-03-07 23:30:40 -0500

    Working tune
    Signed-off-by: Adam Obeng <github@binaryeagle.com>

--- triumph.py
+++ triumph.py
@@ -3,32 +3,20 @@ from time import sleep
 import subprocess
 import midi
 
-def c(x):
-    return '\033[%sm' % x
-
-
-def b(x):
-    return unichr(0x2580 + (x % 20))
-
-
-
-def say(t, r=220):
+def say(t):
     #  http://stackoverflow.com/questions/3516007/run-process-and-dont-wait
-    subprocess.call(['say', t, '-r', str(r)])
+    subprocess.call(['say', t, '-v', 'Vicki'])
 
-
-if 'i' not in locals():
-    i = 0
-i = i + 1
-w = ['INTERGALACTIC', 'PLANETARY', 'PLANETARY', 'INTERGALACTIC']
-
-#m = midi.read_midifile('./jonathan_coulton-still_alive.mid')
-m = midi.read_midifile('./mary.mid')
+m = midi.read_midifile('./jonathan_coulton-still_alive.mid')
 notes = list(i for i in m[1] if isinstance(i, midi.NoteOnEvent))
+
+#m = midi.read_midifile('./mary.mid')
+#notes = list(i for i in m[1] if isinstance(i, midi.NoteOnEvent))
 
 seq = []
 noteon = None
 pos = 0
+# TODO This monofies the track
 for n in notes:
     pos = pos + n.tick
     if not noteon and (n.data[1] != 0):
@@ -38,30 +26,19 @@ for n in notes:
         seq.append((noteon.tick, noteon.data, pos-noteonpos))
         noteon = None
 
-    #if noteon and (on.data[1] != 0) and (on.data[0] != noteon.data[0]): 
-        #continue
-    #else:
-        #noteon = on.data
+    
+lyrics = "This was a triumph. I'm making a note here: HUGE SUCCESS.  It's hard to overstate my satisfaction.".split()
+#lyrics = "Mary had a little lamb, little lamb, little lamb".split()
 
-    #off = list(i for i in notes[ion:] if i.data[0] == n.data[0])
-    #if not off: continue
-    #off = off[0]
-    #nlen = off.tick-on.tick
-    #if nlen != 0:
-        #print ( on.tick, on.data, nlen)
-        #seq.append(( on.tick, on.data, nlen))
-
-seq = [
-        (0, [midi.B_4, 100], 200),
-        (0, [midi.A_4, 100], 200),
-        (0, [midi.G_4, 100], 200),
-        (0, [midi.A_4, 100], 200),
-        (0, [midi.B_4, 100], 200),
-        (0, [midi.B_4, 100], 200),
-        (0, [midi.B_4, 100], 200),
-        ]
-
-for i in seq:
-    print i, '[[pbas %s]] [[inpt phon]] [[volm %s]] uw' % tuple(i[1])
+tosay = ''
+for i,j in zip(seq[:len(lyrics)], lyrics):
     #say('[[pbas %s]] test' % i.data[0], r = 1/(0.01 + i.tick * 0.005))
-    say('[[pbas %s]] [[inpt phon]] [[volm %s]] UW' % tuple(i[1]), r=100*(220.0/i[2]))
+    #say('[[pbas %s]] [[inpt phon]] [[volm %s]] UW' % tuple(i[1]), r=100*(220.0/i[2]))
+    speed = 500*(220.0/i[2])
+    note = [i[1][0]-12, i[1][1]]
+    #tosay +='[[ctxt WORD]] [[pmod 0]] [[pbas %s]] [[volm %s]] [[rate %s]] %s' % tuple(note + [speed,j])
+    tosay +='[[inpt PHON]] [[pbas %s]] [[volm %s]] [[rate %s]] %s' % tuple(note + [speed,'UW,'])
+
+say(tosay)
+
+say(' '.join(lyrics))
